6f08ad29c851d2c960c541dd93779a3075ef37a2
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-04-27 00:09:00 -0500

    (feat): /reportmenu command opens new report menu. functionality in the works

--- web/src/components/ReportMenu.tsx
+++ web/src/components/ReportMenu.tsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from "react";
 //import "./index.css"
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { fetchNui } from "../utils/fetchNui";
+import { Textarea } from "@mantine/core";
+import "../index.scss"
+
+
 
 export default function ReportMenu() {
     const [reportMessage, setReportMessage] = useState("")
 
     const handleCloseMenu = () => {
-      fetchNui('hideFrame')
+      fetchNui('hideReportMenu')
     }
 
     const handleSubmitReport = () => {
@@ -24,13 +28,19 @@ export default function ReportMenu() {
     return (
         <div className="report-menu">
           <h2>Submit a Report </h2>
+          {/* <Textarea 
+          resize='vertical' 
+          placeholder="Write a short description about your issue..."
+          value={reportMessage}
+          onChange={(e) => setReportMessage(e.target.value)}
+          />           */}
           <textarea
             value={reportMessage}
             onChange={(e) => setReportMessage(e.target.value)}
             placeholder="Describe the issue..."
           />
           <button onClick={handleSubmitReport}>Submit Report</button>
-          <button onClick={handleCloseMenu}>X</button>
+          <button className="close-report-menu" onClick={handleCloseMenu}>X</button>
         </div>
       );
 }
