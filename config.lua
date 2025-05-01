de2e50fb656d3f0b8429d463dc71b7c17c40f7c2
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-04-30 19:04:13 -0500

    updating config

--- config.lua
+++ config.lua
@@ -15,7 +15,7 @@ Config.DiscordLink = 'https://discord.gg/CUX8hVnswZ'
 Config.Framework = 'qb'
 
 -- Set Discord Webhook to the channel you want the admin actions to be logged to (recommended this is a private and restricted channel)
-Config.DiscordWebhook = "https://discord.com/api/webhooks/1360063633295675392/s4yWn0IoQYSUIq45016fy85yvuShzvjPd-9FzRm4XQb-_uqLnC11iUNdyswsZUIW9jmh"
+Config.DiscordWebhook = ""
 
 --Only set this if you use a supported ban system.
 --      What does this mean?
